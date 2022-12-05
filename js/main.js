def579fbb586d56003f7108cc7eae35181f5e03a
AlexBarczak <alex.bar074@gmail.com>
2022-12-05 18:28:55 +0000

    creating and appending the top deal items -- broken

--- js/main.js
+++ js/main.js
@@ -1,15 +1,3 @@
-$(function() {
-  $('.deals-slider').slick({
-    slidesToShow: 5,
-    slidesToScroll: 5,
-    autoplay: true,
-    autoplaySpeed: 4000,
-    speed: 1000,
-    prevArrow: '<button type="button" class="slick-prev"><i class="fa-solid fa-circle-chevron-left"></i></button>',
-    nextArrow: '<button type="button" class="slick-next"><i class="fa-solid fa-circle-chevron-right"></i></i></button>',
-  });
-})
-
 function createNode(element) {
   return document.createElement(element);
 }
@@ -19,11 +7,12 @@ function append(parent, el) {
 }
 
 
-url = "https://api.rawg.io/api/games?key=0dac4fa06f3b451ea15e3e67b4b187c2";
-
+//fetch CAROUSEL
 
 //how many items do you want in the carousel?
 var carouselItems = 5;
+
+url = "https://api.rawg.io/api/games?key=0dac4fa06f3b451ea15e3e67b4b187c2&page_size=" + carouselItems;
 
 fetch(url)
     .then((resp) => resp.json())
@@ -76,3 +65,68 @@ fetch(url)
     .catch(function(error) {
         console.log(error);
     });
+
+
+//fetch TOP DEALS
+
+//how many items?
+var topDealItemCount = 20;
+
+url = "https://www.cheapshark.com/api/1.0/deals?pageSize=" + topDealItemCount;
+
+fetch(url)
+    .then((resp) => resp.json())
+    .then(function(data) {
+      console.log(data);
+
+      slider = document.getElementsByClassName("deals-slider")[0];
+
+      for(var i = 0; i < topDealItemCount; i++){
+
+        var item = createNode("div");
+        var img = createNode("image");
+        var title = createNode("h5");
+        var price = createNode("p");
+
+        item.setAttribute('class',"deals-slider__item" );
+        img.setAttribute('class', "slider__item-img");
+        img.setAttribute('src', data[i].thumb);
+        title.setAttribute('class', "slider__item-title mt-3 mb-0");
+        title.innerHTML = data[i].title;
+        price.setAttribute('class', "slider__item-price mt-2");
+        price.innerHTML = "£<span id=\"top-deals-price\">" + data[i].salePrice + "</span>";
+
+        append(item, img);
+        append(item, title);
+        append(item, price);
+
+        append(item, slider);
+
+        console.log(item);
+      }
+
+
+      $(function() {
+        $('.deals-slider').slick({
+          slidesToShow: 5,
+          slidesToScroll: 5,
+          autoplay: true,
+          autoplaySpeed: 4000,
+          speed: 1000,
+          prevArrow: '<button type="button" class="slick-prev"><i class="fa-solid fa-circle-chevron-left"></i></button>',
+          nextArrow: '<button type="button" class="slick-next"><i class="fa-solid fa-circle-chevron-right"></i></i></button>',
+        });
+      })
+    })
+    .catch(function(error) {
+        console.log(error);
+    });
+
+
+// <div class="deals-slider__item">
+//   <img class="slider__item-img" src="https://picsum.photos/180/230" alt="Game logo">
+//   <h5 class="slider__item-title mt-3 mb-0">Game title</h5>
+//   <p class="slider__item-price mt-2">£<span id="top-deals-price">25.99</span></p>
+// </div>
+
+
